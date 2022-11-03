58c9cf28c1164dc06bd2a07d99b171df1051ae70
Alvaro Riquelme Villa <apriquelmev@falabella.cl>
2022-11-03 19:18:37 -0300

    2do commit

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -7,38 +7,53 @@
 <br>
 <div class = "container" id="contacto-form">
     <br>
+   <div class ="container" style="background-image:url('/tucomexlatam/public/img/comercio-internacional.png');"> 
     <div class = "row" style="text-align:center">
-        <h2>Formulario de contacto </h2>
+        <h2>CONTÁCTANOS</h2>
     </div>
+    <br>
+    <div class="row" Style="text-align:center;"></i>
+      <p>TUCOMEXLATAM Posee la experiencia necesaria para entregar un  servicio de calidad, personalizado y eficiente.</p>
+      <p>No te compliques más con los procesos de importación y exportación de productos y/o servicios, consúltanos para que obtengas la asesoría necesaria y no tengas inconvenientes con plazos, cobros extra y retiros de mercancía.</p>  
+    </div>
+</div>
+    <br>
     <div class="mb-3">
-         <label for="exampleFormControlInput1" class="form-label">Correo Electronico</label>
+         <label for="exampleFormControlInput1" class="form-label">Correo Electrónico *</label>
          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="correo@ejemplo.com">
     </div>
     <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Nombre</label>
+        <label for="exampleFormControlInput1" class="form-label">Nombre *</label>
         <input type="nombre" class="form-control" id="exampleFormControlInput1" placeholder="Nombre">
     </div>
     <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Apellido</label>
+        <label for="exampleFormControlInput1" class="form-label">Apellido *</label>
         <input type="apellido" class="form-control" id="exampleFormControlInput1" placeholder="Apellido">
     </div>
-    <div class="row">
-        <div class="col">
-            <h5>Telefono</h5>
-        </div>
-            <div class="col-auto" style=" width: 250px;">
-                <label for="phone" class="visually-hidden">Telefono</label>
-                <input type="tel" class="form-control" id="phone" placeholder="Telefono" style="margin-bottom:8px;">
-            </div>
-    </div>
-    <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Razon Social</label>
+   
+   
+
+    <div class="form-group row" style="margin-top:20px;">
+                <label for="nombre" class="col-form-label col-sm-3" style="width:120px;">Teléfono de contacto *</label>
+              <div class="col-sm-8">
+            <input type="number" name="celular" id="nombre" class="form-control" placeholder="Celular" tabindex="1" style="width: 200px;">
+              </div>
+     </div>
+
+    
+
+     <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Razón Social (Persona natural) *</label>
         <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Razon Social">
     </div>
-    <div class="mb-3" style="width: 250px;">
-        <label for="exampleFormControlInput1" class="visually-hidden">Rut</label>
-        <input type="number" onkeypress="return onlyNumberKey(event)" class="form-control" id="number" placeholder="Rut">
-    </div>
+    <div class="form-group row" style="margin-top:20px;">
+                <label for="nombre" class="col-form-label col-sm-2" style="width:80px;">RUT *</label>
+              <div class="col-sm-8">
+            <input type="number" name="rut" id="nombre" class="form-control" placeholder="Rut" tabindex="1" style="width: 200px;">
+              </div>
+     </div>
+
+     <br>
     
     <h6>Servicio que solicita*</h6>
     <br>
@@ -58,17 +73,25 @@
              <option selected>Selecciona tu servicio</option>
              <option value="1">En proceso de compra</option>
              <option value="2">En transporte</option>
-             <option value="3">URGENTE</option>
+             <option value="3">Urgente</option>
              <option value="3">En planificacion</option>
              <option value="3">Otros</option>
            </select>
     </div>
 
     
-    <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">Servicios</label>
+    <div class="mb-3" style="margin-top:30px;">
+        <label for="exampleFormControlTextarea1" class="form-label">Para una mejor respuesta añada fechas y detalle de productos y/o servicios.</label>
         <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
     </div>
+
+    <div class="mb-3">
+         <label for="formFile" class="form-label">Añada los documentos de transporte, facturas, u otros relevantes para agilizar su trámite.</label>
+         <input class="form-control" type="file" id="formFile">
+    </div>
+
+    <br>
+
     <button type="button" class="btn btn-success">Enviar</button>
 </div>
 <br>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -26,5 +26,5 @@
             <script src="js/carrousel.js"></script>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </html>
